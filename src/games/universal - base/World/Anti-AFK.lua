79d61514dd08b498806389da2f8fd62493cc0e38
stav <126665568+sstvskids@users.noreply.github.com>
2026-06-28 19:28:25 +1000

    Update Anti-AFK.lua

--- src/games/universal - base/World/Anti-AFK.lua
+++ src/games/universal - base/World/Anti-AFK.lua
@@ -1,10 +1,11 @@
-local connections = {}
+local AntiAFK
+--local connections = {}
 
-vape.Categories.World:CreateModule({
+AntiAFK = vape.Categories.World:CreateModule({
 	Name = 'Anti-AFK',
 	Function = function(callback)
 		if callback then
-			for _, v in getconnections(lplr.Idled) do
+			--[[for _, v in getconnections(lplr.Idled) do
 				table.insert(connections, v)
 				v:Disable()
 			end
@@ -12,8 +13,13 @@ vape.Categories.World:CreateModule({
 			for _, v in connections do
 				v:Enable()
 			end
-			table.clear(connections)
+			table.clear(connections)]]
+
+			AntiAFK:Clean(lplr.Idled, function()
+				VirtualUser:CaptureController()
+  				VirtualUser:ClickButton2(Vector2.new())
+			end
 		end
 	end,
-	Tooltip = 'Lets you stay ingame without getting kicked'
+	Tooltip = 'Lets you stay ingame without getting kicked\nSimulates a mouse click'
 })
